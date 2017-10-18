5ca1c5dcb664ad0c9ba4388bd18cee29d9260005
Felipe Rojas Alfonso <frojas@macbook.local>
2017-10-18 15:30:37 +0200

    Initial commit

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,27 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Felipe Rojas Alfonso on 16/10/17.
+//  Copyright © 2017 Red. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        
+    }
+
+}
